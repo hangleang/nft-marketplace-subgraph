0bce47fbf5cc1f4b3c5d31ccd8a084ec63a4d27e
hangleang <hangleang127@gmail.com>
2023-02-18 11:51:29 +0700

    feat(snapshot): add collection snapshot

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,6 +2,12 @@ import { Address, BigDecimal, BigInt } from "@graphprotocol/graph-ts";
 
 export const ZERO_BIGINT: BigInt = BigInt.zero();
 export const ZERO_DECIMAL: BigDecimal = BigDecimal.zero();
+export const HUNDRED_DECIMAL = BigInt.fromI32(100).toBigDecimal();
+export const MAX_DECIMAL: BigDecimal = BigInt.fromI32(i32.MAX_VALUE).toBigDecimal();
 export const ONE_BIGINT: BigInt = BigInt.fromU32(1);
 export const NULL_ADDRESS: Address = Address.zero();
 export const UNKNOWN: string = "UNKNOWN";
+export const SECONDS_PER_DAY = 24 * 60 * 60;
+export const MANTISSA_FACTOR = BigInt.fromI32(10)
+  .pow(18 as u8)
+  .toBigDecimal();
